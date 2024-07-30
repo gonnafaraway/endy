6750e9f60ee59a7b535ffb3f763de20e2b8ff6c7
w0i <>
2024-07-30 22:28:33 +0300

    feat(endy): add benchmark mode

--- e2e.go
+++ e2e.go
@@ -5,15 +5,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"gopkg.in/yaml.v3"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"time"
 
 	"github.com/fatih/color"
 	"go.uber.org/zap"
-	"gopkg.in/yaml.v3"
 )
 
 type Tester struct {
@@ -23,8 +24,9 @@ type Tester struct {
 }
 
 type Config struct {
-	Path    string
-	Timeout time.Duration
+	Path      string
+	Timeout   time.Duration
+	BenchMode bool
 }
 
 type Header struct {
@@ -39,27 +41,9 @@ type Test struct {
 	Timeout    time.Duration `yaml:"timeout,omitempty"`
 	Headers    []Header      `yaml:"headers,omitempty"`
 	Body       string        `yaml:"body,omitempty"`
-}
-
-func New() *Tester {
-	lg, err := zap.NewProduction(
-		zap.WithCaller(false),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return &Tester{
-		Logger: lg,
-		Config: &Config{},
-	}
-}
-
-func (t *Tester) SetTimeout(timeout time.Duration) {
-	t.Config.Timeout = timeout
-}
-
-func (t *Tester) SetConfigPath(path string) {
-	t.Config.Path = path
+	Threads    string        `yaml:"threads,omitempty"`
+	Duration   string        `yaml:"duration,omitempty"`
+	Requests   string        `yaml:"requests,omitempty"`
 }
 
 func (t *Tester) Run() error {
@@ -81,6 +65,7 @@ func (t *Tester) Run() error {
 	if err = yaml.Unmarshal(yamlFile, &tests); err != nil {
 		return fmt.Errorf("unmarshall test configuration: %v", err)
 	}
+
 	t.Tests = tests
 
 	if t.Config.Timeout != 0 {
@@ -94,52 +79,156 @@ func (t *Tester) Run() error {
 
 	client := &http.Client{}
 
-	for _, test := range *t.Tests {
-		var (
-			buf bytes.Buffer
-		)
-
-		if &test.Body != nil {
-			err := json.NewEncoder(&buf).Encode(test.Body)
+	if t.Config.BenchMode {
+		color.Yellow("Running benchmark mode")
+		for _, test := range *t.Tests {
+			err = execBenchTest(t, test)
 			if err != nil {
-				t.Logger.Error("json encode", zap.Error(err))
+				t.Logger.Error("execute test", zap.String("url", test.URL), zap.Error(err))
 				return err
 			}
 		}
+		return nil
+	}
 
-		req, err := http.NewRequestWithContext(ctx, test.Method, test.URL, &buf)
+	color.Green("Running API testing mode")
+	for _, test := range *t.Tests {
+		err = execAPITests(ctx, t, client, test)
 		if err != nil {
-			t.Logger.Error("create http request", zap.Error(err))
 			return err
-		}
-
-		resp, err := client.Do(req)
-		if err != nil {
-			t.Logger.Error("send http request", zap.Error(err))
-			return err
-		}
-		defer func(Body io.ReadCloser) {
-			err = Body.Close()
-			if err != nil {
-				t.Logger.Error("close http body", zap.Error(err))
-			}
-		}(resp.Body)
-
-		bodyBytes, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Logger.Error("read response body", zap.Error(err))
-			return err
-		}
-		bodyString := string(bodyBytes)
-
-		switch {
-		case resp.StatusCode == test.AssertCode:
-			color.Green("Test passed")
-			t.Logger.Info("Test passed", zap.String("url", test.URL), zap.String("method", test.Method), zap.String("body", buf.String()))
-		default:
-			color.Red("Test failed")
-			t.Logger.Fatal("Test failed", zap.String("url", test.URL), zap.String("method", test.Method), zap.String("body", buf.String()), zap.Int("status_code", resp.StatusCode), zap.Error(err), zap.String("response_body", bodyString))
 		}
 	}
+
 	return nil
+}
+
+func New() *Tester {
+	lg, err := zap.NewProduction(
+		zap.WithCaller(false),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &Tester{
+		Logger: lg,
+		Config: &Config{},
+	}
+}
+
+func execAPITests(ctx context.Context, t *Tester, client *http.Client, test Test) error {
+	var (
+		buf bytes.Buffer
+	)
+
+	if &test.Body != nil {
+		err := json.NewEncoder(&buf).Encode(test.Body)
+		if err != nil {
+			t.Logger.Error("json encode", zap.Error(err))
+			return err
+		}
+	}
+
+	req, err := http.NewRequestWithContext(ctx, test.Method, test.URL, &buf)
+	if err != nil {
+		t.Logger.Error("create http request", zap.Error(err))
+		return err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Logger.Error("send http request", zap.Error(err))
+		return err
+	}
+	defer func(Body io.ReadCloser) {
+		err = Body.Close()
+		if err != nil {
+			t.Logger.Error("close http body", zap.Error(err))
+		}
+	}(resp.Body)
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Logger.Error("read response body", zap.Error(err))
+		return err
+	}
+	bodyString := string(bodyBytes)
+
+	switch {
+	case resp.StatusCode == test.AssertCode:
+		color.Green("Test passed")
+		t.Logger.Info("Test passed", zap.String("url", test.URL), zap.String("method", test.Method), zap.String("body", buf.String()))
+	default:
+		color.Red("Test failed")
+		t.Logger.Fatal("Test failed", zap.String("url", test.URL), zap.String("method", test.Method), zap.String("body", buf.String()), zap.Int("status_code", resp.StatusCode), zap.Error(err), zap.String("response_body", bodyString))
+	}
+	return nil
+}
+
+func execBenchTest(t *Tester, test Test) error {
+	var (
+		buf bytes.Buffer
+	)
+
+	if &test.Body != nil {
+		err := json.NewEncoder(&buf).Encode(test.Body)
+		if err != nil {
+			t.Logger.Error("json encode", zap.Error(err))
+			return err
+		}
+	}
+
+	headers := prepareBenchHeaders(test.Headers)
+
+	app := "bombardier"
+	arg0 := "-c"
+	arg1 := test.Threads
+	arg2 := "-n"
+	arg3 := test.Requests
+	arg4 := "-m"
+	arg5 := test.Method
+	arg6 := "-b"
+	arg7 := test.Body
+	arg8 := "-k"
+	arg9 := "-d"
+	arg10 := test.Duration
+	arg11 := "-H"
+	arg12 := headers
+	arg13 := "-p"
+	arg14 := "r"
+	arg15 := test.URL
+
+	cmd := exec.Command(app, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15)
+
+	t.Logger.Info("benchmarking", zap.String("url", test.URL), zap.String("command", cmd.String()))
+
+	stdout, err := cmd.Output()
+	if err != nil {
+		t.Logger.Fatal("bench command failed", zap.Error(err), zap.String("output", string(stdout)))
+	}
+
+	t.Logger.Info("benchmark output", zap.String("output", string(stdout)))
+
+	color.Green("benchmark passed")
+
+	return nil
+}
+
+func (t *Tester) SetTimeout(timeout time.Duration) {
+	t.Config.Timeout = timeout
+}
+
+func (t *Tester) SetConfigPath(path string) {
+	t.Config.Path = path
+}
+
+func (t *Tester) SetBenchmarkMode() {
+	t.Config.BenchMode = true
+}
+
+func prepareBenchHeaders(headers []Header) string {
+	var headerString string
+	for _, header := range headers {
+		headerString += header.Name + ": " + header.Value + " "
+	}
+	return headerString
 }
---
+++ examples/bench/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gonnafaraway/endy"
+	"os"
+	"time"
+)
+
+func main() {
+	e := endy.New()
+
+	e.SetTimeout(10 * time.Second)
+
+	e.SetConfigPath("config.yaml")
+
+	e.SetBenchmarkMode()
+
+	if err := e.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
